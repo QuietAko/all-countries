2d27eb6b54e5170e9fb05ff3c9fa7f74613997ba
Alexander <38661777+QuietAko@users.noreply.github.com>
2025-02-21 23:36:51 +0300

    add redis

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"all-countries/controller"
 	"all-countries/db"
 	"all-countries/repository"
 	"all-countries/service"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
@@ -17,11 +22,25 @@ func main() {
 	}
 	defer db.Close()
 
-	countryRepo := repository.NewCountryRepository(db)
+	// Подключаемся к Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_DSN"), // Адрес Redis-сервера
+		Password: "",                     // Пароль (если есть)
+		DB:       0,                      // Номер базы данных
+	})
+
+	// Проверяем подключение к Redis
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if _, err := redisClient.Ping(ctx).Result(); err != nil {
+		log.Fatalf("Ошибка при подключении к Redis: %v", err)
+	}
+
+	countryRepo := repository.NewCountryRepository(db, redisClient)
 	countryService := service.NewCountryService(countryRepo)
 	countryController := controller.NewCountryController(countryService)
 
-	http.HandleFunc("/countries", countryController.GetAllCountries)
+	http.HandleFunc("api/countries", countryController.GetAllCountries)
 
 	log.Println("Сервер запущен на http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- repository/country_repository.go
+++ repository/country_repository.go
@@ -2,18 +2,38 @@ package repository
 
 import (
 	"all-countries/entity"
+	"context"
 	"database/sql"
+	"encoding/json"
+	"log"
+	"time"
+	"github.com/go-redis/redis/v8"
 )
 
 type CountryRepository struct {
-	db *sql.DB
+	db    *sql.DB
+	redis *redis.Client
 }
 
-func NewCountryRepository(db *sql.DB) *CountryRepository {
-	return &CountryRepository{db: db}
+func NewCountryRepository(db *sql.DB, redis *redis.Client) *CountryRepository {
+	return &CountryRepository{
+		db:    db,
+		redis: redis,
+	}
 }
 
 func (r *CountryRepository) FindAll() ([]entity.Country, error) {
+	ctx := context.Background()
+
+	cached, err := r.redis.Get(ctx, "countries").Result()
+	if err == nil {
+		var countries []entity.Country
+		if err := json.Unmarshal([]byte(cached), &countries); err == nil {
+			log.Println("Cache hit")
+			return countries, nil
+		}
+	}
+
 	rows, err := r.db.Query("SELECT id, name FROM country")
 	if err != nil {
 		return nil, err
@@ -26,8 +46,15 @@ func (r *CountryRepository) FindAll() ([]entity.Country, error) {
 		if err := rows.Scan(&c.ID, &c.Name); err != nil {
 			return nil, err
 		}
-		countries = append(countries, c)
+		countries = append(countries, c) // Используем значение, а не указатель
 	}
+
+	// Сохраняем данные в Redis
+	data, err := json.Marshal(countries)
+	if err != nil {
+		return nil, err
+	}
+	r.redis.Set(ctx, "countries", data, 5*time.Minute)
 
 	return countries, nil
 }
